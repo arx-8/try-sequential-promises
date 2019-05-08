526f25f525294605204d29238665bf3827ff49ba
arx-8 <swans.randomness@gmail.com>
2019-05-09 01:04:31 +0900

    fix

--- src/PromiseUtil.ts
+++ src/PromiseUtil.ts
@@ -7,15 +7,14 @@ export const sequential = async <T>(
   }
 
   const results: T[] = []
-
-  // 末尾に空のPromiseがないと、最後のPromiseの結果をresultsにpushできないため
-  const adjusted = promises.concat(() => Promise.resolve(undefined as any))
-
-  await adjusted.reduce(async (prev, next) => {
-    const res = await prev
-    results.push(res)
-    return next()
-  }, Promise.resolve(first()))
+  await promises
+    // 末尾に空のPromiseがないと、最後のPromiseの結果をresultsにpushできないため
+    .concat(() => Promise.resolve(undefined as any))
+    .reduce(async (prev, next) => {
+      const res = await prev
+      results.push(res)
+      return next()
+    }, Promise.resolve(first()))
 
   return results
 }
