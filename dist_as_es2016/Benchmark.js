65585a9ec749be12af7ba59feaf28c89d1b59400
arx-8 <swans.randomness@gmail.com>
2019-05-18 21:47:17 +0900

    replace benchmark.js
    ベンチマーク対象コードの以外の差分をなくすため、トランスパイル結果をes2017に統一

--- dist_as_es2016/Benchmark.js
+++ dist_as_es2016/Benchmark.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -34,22 +26,22 @@ benchmark_1.default.options.minSamples = 100;
 let logStr = "";
 const suite = new benchmark_1.default.Suite();
 suite
-    .add("Warm-up", () => __awaiter(this, void 0, void 0, function* () {
-    yield testFunc();
-}))
+    .add("Warm-up", async () => {
+    await testFunc();
+})
     // add cases
-    .add("sequentialReduce", () => __awaiter(this, void 0, void 0, function* () {
-    yield sequentialReduce_1.sequentialReduce(testData);
-}))
-    .add("sequentialReduceNoAsync", () => __awaiter(this, void 0, void 0, function* () {
-    yield sequentialReduceNoAsync_1.sequentialReduceNoAsync(testData);
-}))
-    .add("sequentialForOf", () => __awaiter(this, void 0, void 0, function* () {
-    yield sequentialForOf_1.sequentialForOf(testData);
-}))
-    .add("sequentialWhile", () => __awaiter(this, void 0, void 0, function* () {
-    yield sequentialWhile_1.sequentialWhile(testData);
-}))
+    .add("sequentialReduce", async () => {
+    await sequentialReduce_1.sequentialReduce(testData);
+})
+    .add("sequentialReduceNoAsync", async () => {
+    await sequentialReduceNoAsync_1.sequentialReduceNoAsync(testData);
+})
+    .add("sequentialForOf", async () => {
+    await sequentialForOf_1.sequentialForOf(testData);
+})
+    .add("sequentialWhile", async () => {
+    await sequentialWhile_1.sequentialWhile(testData);
+})
     // add listeners
     /**
      * @see https://benchmarkjs.com/docs#options_onCycle
--- dist_as_es5/Benchmark.js
+++ dist_as_es5/Benchmark.js
@@ -1,57 +1,21 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
-var benchmark_1 = __importDefault(require("benchmark"));
-var fs_1 = __importDefault(require("fs"));
-var sequentialForOf_1 = require("./sequentialForOf");
-var sequentialReduce_1 = require("./sequentialReduce");
-var sequentialReduceNoAsync_1 = require("./sequentialReduceNoAsync");
-var sequentialWhile_1 = require("./sequentialWhile");
+const benchmark_1 = __importDefault(require("benchmark"));
+const fs_1 = __importDefault(require("fs"));
+const sequentialForOf_1 = require("./sequentialForOf");
+const sequentialReduce_1 = require("./sequentialReduce");
+const sequentialReduceNoAsync_1 = require("./sequentialReduceNoAsync");
+const sequentialWhile_1 = require("./sequentialWhile");
 console.log("Start benchmark");
-var sleep = function (ms) {
-    return new Promise(function (resolve) { return setTimeout(resolve, ms); });
+const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 };
-var testFunc = function () { return sleep(10); };
-var testData = [];
-for (var index = 0; index <= 100; index += 1) {
+const testFunc = () => sleep(10);
+const testData = [];
+for (let index = 0; index <= 100; index += 1) {
     testData.push(testFunc);
 }
 console.log("Created test data");
@@ -59,77 +23,42 @@ console.log("Created test data");
  * @see https://github.com/bestiejs/benchmark.js/issues/172
  */
 benchmark_1.default.options.minSamples = 100;
-var logStr = "";
-var suite = new benchmark_1.default.Suite();
+let logStr = "";
+const suite = new benchmark_1.default.Suite();
 suite
-    .add("Warm-up", function () { return __awaiter(_this, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, testFunc()];
-            case 1:
-                _a.sent();
-                return [2 /*return*/];
-        }
-    });
-}); })
+    .add("Warm-up", async () => {
+    await testFunc();
+})
     // add cases
-    .add("sequentialReduce", function () { return __awaiter(_this, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, sequentialReduce_1.sequentialReduce(testData)];
-            case 1:
-                _a.sent();
-                return [2 /*return*/];
-        }
-    });
-}); })
-    .add("sequentialReduceNoAsync", function () { return __awaiter(_this, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, sequentialReduceNoAsync_1.sequentialReduceNoAsync(testData)];
-            case 1:
-                _a.sent();
-                return [2 /*return*/];
-        }
-    });
-}); })
-    .add("sequentialForOf", function () { return __awaiter(_this, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, sequentialForOf_1.sequentialForOf(testData)];
-            case 1:
-                _a.sent();
-                return [2 /*return*/];
-        }
-    });
-}); })
-    .add("sequentialWhile", function () { return __awaiter(_this, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, sequentialWhile_1.sequentialWhile(testData)];
-            case 1:
-                _a.sent();
-                return [2 /*return*/];
-        }
-    });
-}); })
+    .add("sequentialReduce", async () => {
+    await sequentialReduce_1.sequentialReduce(testData);
+})
+    .add("sequentialReduceNoAsync", async () => {
+    await sequentialReduceNoAsync_1.sequentialReduceNoAsync(testData);
+})
+    .add("sequentialForOf", async () => {
+    await sequentialForOf_1.sequentialForOf(testData);
+})
+    .add("sequentialWhile", async () => {
+    await sequentialWhile_1.sequentialWhile(testData);
+})
     // add listeners
     /**
      * @see https://benchmarkjs.com/docs#options_onCycle
      */
-    .on("cycle", function (event) {
+    .on("cycle", (event) => {
     console.log(String(event.target));
-    logStr += String(event.target) + "\n";
+    logStr += `${String(event.target)}\n`;
 })
     /**
      * @see https://benchmarkjs.com/docs#options_onComplete
      */
     // eslint-disable-next-line func-names
     .on("complete", function () {
-    console.log("Fastest is *** " + this.filter("fastest").map("name") + " ***");
-    logStr += "Fastest is *** " + this.filter("fastest").map("name") + " ***\n";
+    console.log(`Fastest is *** ${this.filter("fastest").map("name")} ***`);
+    logStr += `Fastest is *** ${this.filter("fastest").map("name")} ***\n`;
     // 実行結果をファイルに出力
-    var nowStr = new Date().toISOString().replace(/:/g, "");
-    fs_1.default.writeFileSync("benchmark_" + nowStr + ".txt", logStr);
+    const nowStr = new Date().toISOString().replace(/:/g, "");
+    fs_1.default.writeFileSync(`benchmark_${nowStr}.txt`, logStr);
 })
     .run({ async: true });
